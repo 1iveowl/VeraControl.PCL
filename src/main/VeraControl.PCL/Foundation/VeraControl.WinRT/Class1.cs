e557b40b4f38e4aaf4e9dd31b26f6c5a4e7f8cef
1iveowl <liveowl@outlook.com>
2016-09-11 11:43:09 +0200

    Start

---
+++ src/main/VeraControl.PCL/Foundation/VeraControl.WinRT/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeraControl.WinRT
+{
+    public class Class1
+    {
+    }
+}
