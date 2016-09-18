b5211db7c01cadd21fe15e00a0a595ef86b2bf3a
1iveowl <liveowl@outlook.com>
2016-09-18 21:02:50 +0200

    wip

---
+++ src/main/VeraControl.PCL/Foundation/VeraControl.Shared/Model/UpnpService/TemperatureSensor1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IVeraControl.Model;
+
+namespace VeraControl.Model.UpnpService
+{
+    public class TemperatureSensor1 : IUpnpService
+    {
+        public string ServiceUrn => "urn:schemas-upnp-org:service:TemperatureSensor:1";
+        public string ServiceName => nameof(TemperatureSensor1);
+        public IEnumerable<IUpnpAction> Actions { get; set; }
+        public IEnumerable<IUpnpStateVariable> StateVariables { get; set; }
+    }
+}
---
+++ src/main/VeraControl.PCL/Foundation/VeraControl.Shared/Model/UpnpService/TemperatureSetpoint1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IVeraControl.Model;
+
+namespace VeraControl.Model.UpnpService
+{
+    // Spec: http://upnp.org/specs/ha/UPnP-ha-TemperatureSetpoint-v1-Service.pdf
+    public class TemperatureSetpoint1 : IUpnpService
+    {
+        public string ServiceUrn => "urn:schemas-upnp-org:service:TemperatureSetpoint:1 ";
+        public string ServiceName => nameof(TemperatureSetpoint1);
+        public IEnumerable<IUpnpAction> Actions { get; set; }
+        public IEnumerable<IUpnpStateVariable> StateVariables { get; set; }
+    }
+}
