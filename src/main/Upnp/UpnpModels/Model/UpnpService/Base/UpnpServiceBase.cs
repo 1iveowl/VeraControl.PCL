715d460cc96178dbdafca5bb57da8df15b98ccc1
1iveowl <liveowl@outlook.com>
2016-11-05 11:20:01 +0100

    Migrated to .NET Standard 1.2 and started Upnp Refactoring

---
+++ src/main/Upnp/UpnpModels/Model/UpnpService/Base/UpnpServiceBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using IVeraControl.Model;
+
+
+namespace UpnpModels.Model.UpnpService.Base
+{
+    public abstract class UpnpServiceBase
+    {
+        public IEnumerable<IUpnpAction> Actions { get; set; }
+
+        public IEnumerable<IUpnpStateVariable> StateVariables { get; set; }
+
+        public virtual IUpnpAction LookupAction(dynamic actionName)
+        {
+            return Actions.FirstOrDefault(a => a.ActionName == actionName.ToString());
+        }
+
+        public virtual IUpnpStateVariable LookupStateVariable(dynamic stateVariableName)
+        {
+            var result = StateVariables.FirstOrDefault(v => v.VariableName == stateVariableName.ToString());
+            return result;
+        }
+    }
+}
