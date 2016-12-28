b02587d4a35c67ac30abd4b7d263bba744355577
1iveowl <liveowl@outlook.com>
2016-12-28 13:01:47 +0100

    Added .NET Standard 1.6 lib to nuget

--- src/main/VeraControl.PCL/CrossPlatform/VeraControl.NetStandard16/Properties/AssemblyInfo.cs
+++ src/main/VeraControl.PCL/CrossPlatform/VeraControl.NetStandard16/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IVeraControl.PCL")]
+[assembly: AssemblyTitle("VeraControl.NetStandard16")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IVeraControl.PCL")]
+[assembly: AssemblyProduct("VeraControl.NetStandard16")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.0.0")]
-[assembly: AssemblyFileVersion("0.7.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
